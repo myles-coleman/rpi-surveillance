04e23b5482220bb1a79f431edb89310db832b3f4
Myles Coleman <mylescoleman05@gmail.com>
2024-10-09 19:51:09 -0700

    chore: now using aws sdk

--- server/main.js
+++ server/main.js
@@ -1,30 +1,74 @@
+const { GetObjectCommand, PutObjectCommand, S3Client, S3ServiceException } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { readFile } = require("fs/promises");
 const express = require('express');
-const { exec } = require('child_process');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const port = 3000;
+
+const credentials = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+
+const config = {
+    region: process.env.AWS_REGION,
+    credentials,
+}
+
+const main = async ({ bucketName, key, filePath }) => {
+    const client = new S3Client(config);
+    let url = '';
+    const putObjectCommand = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        Body: await readFile(filePath),
+    });
+
+    try {
+        //record video
+        exec('rpicam-vid --framerate 120 --width 1920 --height 1080 -o video.mp4 -t 3s --level 4.2 --denoise cdn_off -n', (error) => {
+            if (error) {
+            return res.status(500).json({ error: 'Failed to upload video' });
+            }
+            console.log('Video recorded');
+        });
+
+        //upload video to bucket
+        const response = await client.send(putObjectCommand);
+        console.log(`file uploaded: ${response}`);
+
+        //generate url
+        const getObjectCommand = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        });
+        url = await getSignedUrl(client, getObjectCommand, { expiresIn: 3600 });
+
+        //delete video
+        exec('rm video.mp4', (error) => {
+        if (error) {
+            return res.status(500).json({ error: 'Failed to delete video' });
+        }
+        console.log('Video deleted');
+    });
+
+    } catch (caught) {
+        if (caught instanceof S3ServiceException) {
+            throw caught;
+        }
+    }
+    return url;
+};
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-
-  // record and upload video to s3 bucket
-  exec('bash video.sh', (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: 'Failed to upload video' });
-    }
-
-    // generate URL
-    const command = "aws s3 presign s3://rpi-surveillance/video.mp4 --expires-in 3600";
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        return res.status(500).json({ error: 'Failed to generate URL' });
-      }
-      res.json({ url: stdout.trim() });
-    });
-  });
+app.get('/', (res) => {
+    const url = main({ bucketName: "rpi-surveillance", key: "video.mp4", filePath: "video.mp4" });
+    res.json({ url });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
